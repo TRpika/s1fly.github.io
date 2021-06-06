c11febcfd848674dd0a00f6083ea659d0d1fd3f0
TRpika <85185682+TRpika@users.noreply.github.com>
2021-06-07 00:03:55 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,335 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Fly</title>
+<meta charset="utf-8">
+<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux and BSD.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, bsd, open source, nekotekina, kd11, home">
+<meta name="author" content="RPCS3">
+<meta name="copyright" content="RPCS3">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+</head>
+<body>
+<img src="/img/graphics/landing/dualshock3.png" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1b.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2b.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3b.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/ui-main-video.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'RKsLVfST-z8',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'_aEVRkxR08k',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'8FTQgFT6Znw',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+		<div class="wavebar-con-container-master">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						
+						<div class='landing-tx1-heading pulsate'>
+							<h1>Fly</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/Fly2.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-search darkmode-slimbar">
+		<div class="search-ovr-container">
+		</div>
+		
+	</div>
+	
+		<div class="landing-con-contribute darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Создатель игры!</h2>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>Алексей Степаненко сделал игру на Unity, используя язык C#.
+									 <a href="https://www.youtube.com/watch?v=1GCbGhe8Hbc" target="_blank">
+									 	<span>Видео</span>
+									 </a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patr-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Почему мой выбор пал на C#?<h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									На мой взгляд он не очень сложный и в то же время хороший язык программирования.
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Почему мой выбор пал на Unity?<h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									На мой взгляд это лучший игрой движок.
+									Почему:
+									1. Он простой для изучения.
+									2. Удобный интерфейс.
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="git-con-container">
+					<div class="git-con-logo">
+						<div class="git-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Графическй дизайнер!</h2>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									  Александр Рябцев создавал разные модельки для игры в Illustrator с помощью видео уроков.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="pat-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Почему я выбрал Illustrator <h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+								Во время проекта я научился создавать 2D модельки в Illustrator по видио урокам с помощью математических фигур.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="pa-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Нож<h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									Нож создавался с помощью прямоугольников. Сначала я закруглил один край, затем добавил несколько деталей и сделал тень с помощью инструмента создание фигур.
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="p-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Сан Саныч<h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									Я сделал этого персонажа абстрагироваясь от персонажа из интернета.
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="git-con-container">
+					<div class="git-con-logo">
+						<div class="gt-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Создатель сайта!</h2>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p> Я создал этот ужасный сайт используя языки програмирования.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="ped-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Почему я выбрал HTML<h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									Мне давно знаком этот язык уже 4 года. И мне нравиться его функцтонал, он очень большой и удобный.
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+			<div class="landing-con-patreon darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="pd-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra darkmode-txt">
+								<h2>Почему я решил сделать сайт?<h2>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									Мне показалось что сайт будет намного интереснее чем обычная презентация ( я просто не умею работать в Power point).
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/ui-main-footer.php';?>
+</div>
+</body>
+</html>
